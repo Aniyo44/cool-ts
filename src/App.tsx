2f5f0d44c2814f890bc9486b43b37fc4713da183
Aniyo44 <yaf_did@outlook.com>
2023-12-17 20:17:31 +0100

    fc

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,36 @@
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-const supabase = createClient("https://glfcfqqhhippoyqabxpm.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsZmNmcXFoaGlwcG95cWFieHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwODQ2NTEsImV4cCI6MjAwNDY2MDY1MX0.rK9qLQk3GLyi_y3tg5jm20vXBnbmXWibeUyr6BGUoaE");
-
-import './index.css'
+import './index.css';
+import websitesData from "./data";
 
 function App() {
-  const [websites, setWebsites] = useState<any[]>([]);
+  const [randomWebsite, setRandomWebsite] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    getWebsites();
+
+    // Select a random website initially
+    selectRandomWebsite();
   }, []);
 
-  async function getWebsites() {
-    try {
-      const { data, error } = await supabase.from("websites").select();
-      if (error) {
-        throw error;
-      }
-      setWebsites(data || []);
-    } catch (error) {
-      console.error("Error fetching websites:", error);
-    }
-  }
+  const selectRandomWebsite = () => {
+    const randomIndex = Math.floor(Math.random() * websitesData.length);
+    setRandomWebsite(websitesData[randomIndex]);
+  };
 
-  console.log(websites);
+  const handleDiveInClick = () => {
+    // Select a new random website when the user clicks "Dive In"
+    selectRandomWebsite();
+  };
 
   return (
-  <div className='min-h-screen flex justify-center max-w-full items-center flex-col bg-gradient-to-br  from-fuchsia-500 to-yellow-500 '>
-   
-    <div className='text-center'>
-    <h1 className='text-7xl whitespace-pre-line text-white'>Explore <br></br>the Web's Hidden Gems!</h1>
-    </div>
-    
-    <div className='mt-20'>
-    <a href='#' target='_blank' className='text-5xl bg-gradient-to-tr from-blue-500 to-orange-500  rounded-3xl text-center p-3 text-white font-thin italic'>Dive In</a>
-    </div>
+    <div className='min-h-screen flex justify-center max-w-full items-center flex-col bg-gradient-to-br  from-fuchsia-500 to-yellow-500 '>
+      <div className='text-center'>
+        <h1 className='text-7xl whitespace-pre-line text-white'>Explore <br />the Web's Hidden Gems!</h1>
+      </div>
+      <div className='mt-20'>
+        <a href={randomWebsite} target='_blank' rel='noopener noreferrer' onClick={handleDiveInClick} className='text-5xl bg-gradient-to-tr from-blue-500 to-orange-500 rounded-3xl text-center p-3 text-white font-thin italic'>Dive In</a>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,25 @@
+const websitesData: string[] = [
+    'https://quickdraw.withgoogle.com/',
+    'https://www.importyeti.com/',
+    'http://radio.garden/',
+    'https://driveandlisten.herokuapp.com/',
+    'https://musclewiki.com/',
+    'https://mixedname.com/',
+    'https://nobody.live/',
+    'https://thanaverage.xyz/',
+    'https://alexanderperrin.com.au/',
+    'https://neal.fun/life-stats/',
+    'http://listen.hatnote.com/',
+    'https://nearvear.com/',
+    'https://pomodor.app/timer',
+    'https://visualgo.net/en',
+    'https://slices.ovh/',
+    'https://tools.pdf24.org/en/',
+    'https://onlinesafari.tv/',
+    'https://cognitivebias.io/'
+
+
+
+  ];
+  
+  export default websitesData;
